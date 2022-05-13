8a90c04eae29ba077a1e9e624eaf49b4c8c14b4d
Edwin Dominguez <edwindominguez@hotmail.com>
2022-05-12 20:08:25 -0500

    Added spring data jpa dependency, postgresql driver, customer repository, edited customer model and service to save customer

--- customer/src/main/java/com/donnatto/customer/CustomerService.java
+++ customer/src/main/java/com/donnatto/customer/CustomerService.java
@@ -1,9 +1,13 @@
 package com.donnatto.customer;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class CustomerService {
+
+    private final CustomerRepository customerRepository;
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.getFirstName())
@@ -12,6 +16,6 @@ public class CustomerService {
                 .build();
         // todo: check if email is valid
         // todo: check if email is not taken
-        // todo: check if customer is in db
+        customerRepository.save(customer);
     }
 }
